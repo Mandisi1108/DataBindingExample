e831005ae7208adecb08dd47f8774bfb76753684
Mandisi <mandisi.kuse@younglings.africa>
2019-10-03 17:15:12 +0200

    Binding event listeners

--- app/src/main/java/com/mara/databindingexample/MainActivity.java
+++ app/src/main/java/com/mara/databindingexample/MainActivity.java
@@ -3,20 +3,25 @@ package com.mara.databindingexample;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.databinding.DataBindingUtil;
 
+import android.content.Context;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Toast;
 
 import com.mara.databindingexample.databinding.ActivityMainBinding;
 
 public class MainActivity extends AppCompatActivity {
     private ActivityMainBinding activityMainBinding;
+    private ClickHandlers handlers;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
         activityMainBinding = DataBindingUtil.setContentView(this, R.layout.activity_main);
         activityMainBinding.setStudent(getCurrentStudent());
+        handlers = new  ClickHandlers(this);
+        activityMainBinding.setClick(handlers);
     }
 
     private Student getCurrentStudent(){
@@ -24,5 +29,21 @@ public class MainActivity extends AppCompatActivity {
         student.setName("Mandisi");
         student.setEmail("mandisikuse@gmail.com");
         return student;
+    }
+
+    public class ClickHandlers {
+        Context context;
+
+        public ClickHandlers(Context context) {
+            this.context = context;
+        }
+
+        public void onEnrollButtonClicked(View view){
+            Toast.makeText(context,"Enroll clicked", Toast.LENGTH_LONG).show();
+        }
+
+        public void onCancelButtonClicked(View view){
+            Toast.makeText(context,"Cancel clicked", Toast.LENGTH_LONG).show();
+        }
     }
 }
